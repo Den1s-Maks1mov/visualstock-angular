972e8bdbc7502c3076b9322c5c4d390479735a77
Den1s_Maks1mov <maksymov.denys@student.sumdu.edu.ua>
2025-11-10 16:04:20 +0200

    PR-01: Update: Header and Footer implemented as separate Angular components

--- src/app/components/layout/layout.ts
+++ src/app/components/layout/layout.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    Header,
+    Footer
+  ],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
 export class LayoutComponent {
-  appTitle: string = 'VisualStock - discover our world';
+
 }
