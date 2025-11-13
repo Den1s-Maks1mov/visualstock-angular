fe885cba9e474095d81a1728667c38dc730d1576
Den1s_Maks1mov <maksymov.denys@student.sumdu.edu.ua>
2025-11-13 10:30:51 +0200

    PR-02: Update: Finalized Photo model, static components (list/card) and routing setup

--- src/app/features/photos/photo-card/photo-card.ts
+++ src/app/features/photos/photo-card/photo-card.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Photo } from '../../../core/models/photo.interface';
 
 @Component({
   selector: 'app-photo-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './photo-card.html',
   styleUrl: './photo-card.css',
 })
 export class PhotoCard {
-
+  // Чорнетка для статичного відображення
+  photoData: Photo = {
+    id: '0',
+    title: 'Placeholder',
+    author: 'N/A',
+    url: '',
+    views: 0,
+    isPremium: false,
+    tags: ['initial', 'test'],
+    uploadDate: new Date('2024-01-01')
+  };
 }
