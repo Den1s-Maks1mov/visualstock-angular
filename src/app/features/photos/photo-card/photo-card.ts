06690dd88b4c7459347810282442677108292b2b
Den1s_Maks1mov <maksymov.denys@student.sumdu.edu.ua>
2025-11-14 15:27:00 +0200

    PR-03: Dynamic data binding, *ngFor, @Input, *ngIf, and ngClass styling

--- src/app/features/photos/photo-card/photo-card.ts
+++ src/app/features/photos/photo-card/photo-card.ts
@@ -1,23 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import { Photo } from '../../../core/models/photo.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-photo-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './photo-card.html',
   styleUrl: './photo-card.css',
 })
+
 export class PhotoCard {
-  // Чорнетка для статичного відображення
-  photoData: Photo = {
-    id: '1',
-    title: 'Mountain lake Vista',
-    author: 'Nikita2504',
-    pImage: "assets/images/photo-1.jpg",
-    views: 1240,
-    isPremium: false,
-    tags: ['nature', 'landscape'],
-    uploadDate: new Date('2024-06-15')
-  };
+  @Input() photoData!: Photo;
 }
